a3006cb95d9e1a38e21c4c0e036032e184102826
Blen Wondimagegn <mekuriablen@gmail.com>
2020-09-16 21:53:20 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -1,4 +1,4 @@
-CREATE TABLE cats ( id INTEGER PRIMARY KEY, title TEXT, author_id INTEGER, SUB );
+CREATE TABLE series ( id INTEGER PRIMARY KEY, title TEXT, author_id INTEGER, subgenre_id TEXT);
 
 CREATE TABLE cats (
 id INTEGER PRIMARY KEY,
